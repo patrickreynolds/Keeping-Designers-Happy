43237f05285ed043d162687aa5f5b1580c9c07ab
Patrick Reynolds <patrick.reynolds@affirm.com>
2016-04-24 09:17:12 -0700

    Initial Commit

---
+++ Keeping Designers Happy With EnumsTests/Keeping_Designers_Happy_With_EnumsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Keeping_Designers_Happy_With_EnumsTests.swift
+//  Keeping Designers Happy With EnumsTests
+//
+//  Created by Patrick Reynolds on 4/24/16.
+//  Copyright © 2016 Patrick Reynolds. All rights reserved.
+//
+
+import XCTest
+@testable import Keeping_Designers_Happy_With_Enums
+
+class Keeping_Designers_Happy_With_EnumsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
