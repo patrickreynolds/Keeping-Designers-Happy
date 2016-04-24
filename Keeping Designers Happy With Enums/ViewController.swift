5b9b6e2cbbdc1e07ac7f8700308a9dcfabe50309
Patrick Reynolds <patrick.reynolds@affirm.com>
2016-04-24 09:17:12 -0700

    Initial Commit

---
+++ Keeping Designers Happy With Enums/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Keeping Designers Happy With Enums
+//
+//  Created by Patrick Reynolds on 4/24/16.
+//  Copyright © 2016 Patrick Reynolds. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
